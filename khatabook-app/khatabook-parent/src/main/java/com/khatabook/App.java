24a8ff4b83ef5915c9cf00a4948878b922a64265
vijayanandc <vijayanandmku@gmail.com>
2025-03-14 12:50:54 +0530

    Add khatabook-app/khatabook-parent/src/main/java/com/khatabook/App.java 1741936853669

---
+++ khatabook-app/khatabook-parent/src/main/java/com/khatabook/App.java
@@ -0,0 +1,13 @@
+package com.khatabook;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
